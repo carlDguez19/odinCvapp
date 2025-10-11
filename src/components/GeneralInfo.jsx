adb7ad1a826ea9e28b1b95615ee9928faec2cd86
Carlos <110745188+carlDguez19@users.noreply.github.com>
2025-10-10 21:06:21 -0600

    need to create app.jsx where ill pass the info for each section.

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,22 +1,29 @@
 import { use, useState } from "react";
 import { Button } from "./Button";
 
-function GeneralInfo(){
+function GeneralInfo({onSubmit}){
     const[genInfo, setGenInfo] = useState({
         name: "",
         email: "",
         phone: "",
     });
 
-    function generalSubmit(){
-        
+    const handleChange = (e) => {
+        const {id, value} = e.target;
+        setGenInfo(prev => ({ ...prev, [id]: value }));
     }
+
+    function generalSubmit(){
+        e.preventDefault();
+        onSubmit(genInfo);
+    }
+
     return (
         <div className="general-info">
             <h2>General Information</h2>
-            <input type="text" id="name"/>
-            <input type="email" id="email"/>
-            <input type="tel" id="phone"/>
+            <input type="text" id="name" value={genInfo.name} onChange={handleChange}/>
+            <input type="email" id="email" value={genInfo.email} onChange={handleChange}/>
+            <input type="tel" id="phone" value={genInfo.phone} onChange={handleChange}/>
             <Button type="submit" text="Submit" onClick={generalSubmit} fontSize="18px"/>
         </div>
     )
